07c11a4cb78ec382c3899949ea7fe2148209c2da
sam raphael <samraphael@gmail.com>
2023-03-26 10:20:54 -0400

    made async functions + updated getCourse to include populate() calls

--- back-end/db/courses.js
+++ back-end/db/courses.js
@@ -1,21 +1,25 @@
 import {Courses as courses} from './schemas/models.js'
+import Users from "../routes/users.js";
 
-export const createCourse = (course) => {
+export const createCourse = async (course) => {
     return courses.create(course)
 }
 
 
-export const updateCourse = (_id, update) => {
+export const updateCourse = async (_id, update) => {
     return courses.findOneAndUpdate({_id}, update)
 }
 
 
-export const getCourse = (_id) => {
-    return courses.findOne({_id})
+export const getCourse = async (_id,{teachers,students}) => {
+    let query= courses.findOne({_id})
+    if (teachers) query = query.populate('teachers')
+     if (students) query = query.populate('students')
+    return await query.exec()
 
 }
 
-export const getCourses = (sort) => {
+export const getCourses =async  (sort) => {
 
     let courses_= courses.findOne({})
     if (sort) courses_ = courses_.sort(sort)
@@ -24,16 +28,19 @@ export const getCourses = (sort) => {
 }
 
 
-export const deleteCourse = (_id) => {
+export const deleteCourse = async (_id) => {
 return courses.findOneAndDelete({_id})
 }
 
 
-export const teachCourse = (username, _id) => {
-    // return courses.update({_id},{$push:{username:}})///TODO $push
+export const teachCourse = async (username, _id) => {
+    await courses.update({_id},{$push:{teachers:_id}})
+    await Users.update({username}, {$push:{courses:_id}})
 }
 
 
-export const enrollCourse = ()=>{
+export const enrollCourse = async ()=>{
+    await Users.update({username}, {$push:{courses:_id}})
+     await courses.update({_id},{$push:{students:_id}})
 
 }
