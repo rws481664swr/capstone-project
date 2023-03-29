0fa3f689b06072a59ae9f75367cc7564bb5bf56c
sam raphael <samraphael@gmail.com>
2023-03-29 13:18:27 -0400

    error middleware tests

--- back-end/test/tests/misc.test.js
+++ back-end/test/tests/misc.test.js
@@ -2,6 +2,9 @@ import {should as chaiShould,expect} from 'chai'
 import {jsonify} from "../../db/util.js";
 import {c1, u1,default as common} from "../common/seed-test-db.js";
 import {Schema} from "mongoose";
+
+import _404 from '../../middleware/404.js'
+import error from '../../middleware/error.js'
 const should = chaiShould()
 
 describe('jsonify',()=>{
@@ -42,4 +45,59 @@ describe('jsonify',()=>{
     })
     })
 
+})
+describe('404 middleware',()=>{
+    it('',()=>{
+        _404(         {}, {status: (z) => {
+              z.should.equal(404)
+              return {json:
+                    (x) => x.should.eql({status: 404, message: "Not Found"})
+              }
+            }},()=>{})
+    })
+})
+describe('error middleware',()=>{
+    it('',()=>{
+        const expected={status:123,message:'123'}
+         error(expected,{},{
+             status:function (status){
+                 status.should.eql(expected.status);
+             return {json:(obj)=>{
+                     ({status,
+                         ...obj
+                     }).should.eql(expected)}}}},()=>{})
+    })
+    it('',()=>{
+        const expected={status:500,message:'Server Error'}
+
+        error({},{},{
+             status:function (status=500){
+                 status.should.eql(expected.status);
+                 return {json:(obj)=>{
+                         ({status,
+                             ...obj
+                         }).should.eql(expected)}}}},()=>{})
+    })
+    it('',()=>{
+        const expected={status:123}
+
+        error(expected,{},{
+             status:function (status=500){
+                 status.should.eql(expected.status);
+                 return {json:(obj={message:'Server Error'})=>{
+                         ({status,
+                             ...obj
+                         }).should.eql({...expected,message:"Server Error"})}}}},()=>{})
+    })
+    it('',()=>{
+        const expected={message:'123'}
+
+        error(expected,{},{
+             status:function (status=500){
+                 status.should.eql(500);
+                 return {json:(obj={message:'Server Error'})=>{
+                         ({status,
+                             ...obj
+                         }).should.eql({status:500,message:expected.message})}}}},()=>{})
+    })
 })
