084bdb5348d77533dab395a60610a168c8b6e397
sam raphael <samraphael@gmail.com>
2023-04-10 14:20:06 -0400

    auth tests

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -3,29 +3,45 @@ import {login} from "../db/creds.js";
 import {BadRequestError, UnauthorizedError} from "../util/Errors.js";
 import {createUser, getUser} from "../db/users.js";
 import createToken from '../createToken.js'
-import {ensureLoggedIn} from "../middleware/authToken.js";
+import {isLoggedIn} from "../middleware/predicates.js";
 
 const router = express.Router()
 export default router
 
+const notLoggedIn = (req, res, next) => {
 
-router.post('/login', async ({body: {username, password}}, res, next) => {
+}
+
+function ensureLoggedOut(type='log in') {
+    return (req, res, next)=> {
+        try {
+            if (isLoggedIn(res)) {
+                throw new BadRequestError(`Cannot ${type} twice`);
+            }
+            return next();
+        } catch (err) {
+            return next(err);
+        }
+    }
+}
+
+router.post('/login', ensureLoggedOut(), async ({body: {username, password}}, res, next) => {
     try {
         const isLoggedIn = await login(username, password)
         if (!isLoggedIn) throw new UnauthorizedError("Invalid username/password")
-        const {role}= await getUser(username)
-        const token = createToken(username,role)
+        const {role} = await getUser(username)
+        const token = createToken(username, role)
         return res.json({token})
     } catch (e) {
         next(e)
     }
 })
 
-router.post('/register', async ({body}, res, next) => {
+router.post('/register', ensureLoggedOut('register'), async ({body}, res, next) => {
     try {
-        if(!body.password) throw new BadRequestError('Must provide a password')
+        if (!body.password) throw new BadRequestError('Must provide a password')
         const user = await createUser(body)
-        const token = createToken(user.username,user.role)
+        const token = createToken(user.username, user.role)
         res.json({token})
     } catch (e) {
         next(e)
