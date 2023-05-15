6908925b3d2f09f882d16e283a79d887a817e49c
sam raphael <samraphael@gmail.com>
2023-05-15 10:38:24 -0400

    added visible prop to CreatePost

--- frontend/src/components/CourseView/Course.js
+++ frontend/src/components/CourseView/Course.js
@@ -1,30 +1,37 @@
 import {useParams} from "react-router-dom";
 import useGet from "../../hooks/useGet";
 import PostList from "./Posts/PostList";
-import Modal, {useModal} from "../General/Modal";
+import {useModal} from "../General/Modal";
 import {CourseContextProvider} from "../../state/contexts/CourseContext";
 import {Provider} from "react-redux";
 import postsStore from "../../state/redux/postsStore";
-import useToggle from "../../hooks/useToggle";
 import CreatePost from "./CreatePost";
 
-const Course = ({}) => {
+const Course = () => {
     const {id} = useParams()
-    const course = useGet('courses', {id, query: {populate: true}})
+    const query = {populate: true}
+    const course = useGet('courses', {id, query})
     const [visible, {show, hide}] = useModal()
     return course &&
         <CourseContextProvider course={course}>
-            <div className={'Course'}>
-                {!visible && <button onClick={show}>Create a post</button>}
+            <div id={'Course'}>
+                <div className={'d-flex justify-content-between css.courseHeader'}>
+                <div id={'welcome'} >Welcome to {course.courseName}</div>
+                    {!visible &&
+                        <button id={'postbutton'} className={` createPostButton}`} onClick={show}>
+                            Create a post <i className="fa  fa-plus"></i>
 
-            <div>COURSE BEGIN</div>
-            {/*<div>COURSE {id} - {JSON.stringify(course)} </div>*/}
-            {Object.keys(course).join(' ')}
-            <div>COURSE END</div></div>
+                        </button>
+                    }
+                </div>
+                {/*<div>COURSE {id} - {JSON.stringify(course)} </div>*/}
+                {Object.keys(course).join(' ')}
+                <div>COURSE END</div>
+            </div>
             <div>PostList BEGIN</div>
             <div>
                 <Provider store={postsStore}>
-                    <CreatePost hide={hide} visible={visible} course={course}/>
+                    {visible&& <CreatePost hide={hide} visible={visible} course={course}/>}
 
                     <PostList course_id={id}/>
                 </Provider>
--- frontend/src/components/CourseView/CreatePost/index.js
+++ frontend/src/components/CourseView/CreatePost/index.js
@@ -1,4 +1,3 @@
-import {useState} from "react";
 import useForm from "../../../hooks/useForm";
 import useFlash from "../../../hooks/useFlash";
 import {useGlobalContext} from "../../../state/contexts/GlobalContext";
@@ -6,19 +5,21 @@ import useAxios from "../../../api";
 import {ADD} from "../../../state/actions/posts";
 import {useDispatch} from "react-redux";
 import Modal from "../../General/Modal";
+import {useCallback} from "react";
 
 
-function useSubmitPost(course, form, clear,hide) {
+function useSubmitPost(course, form, clear, hide) {
     const dispatch = useDispatch()
-
-    const axios = useAxios()
+    const {post:postRequest} = useAxios()
     const {_id: user, username} = useGlobalContext()
     const [msg, flash, setCss] = useFlash()
-    const submit = async (e) => {
+    const submit = useCallback(
+    async (e) => {
         e.preventDefault()
         const danger = () => setCss('text-danger')
         const post = {...form, course: course._id, user, username}
-        if(!post.content.trim() && !post.title.trim()) {
+        console.log('post', post)
+        if (!post.content.trim() && !post.title.trim()) {
             danger()
             return flash('Title and content are required')
         }
@@ -30,27 +31,33 @@ function useSubmitPost(course, form, clear,hide) {
             danger()
             return flash('Content is required')
         }
-        setCss('text-success')
-        const payload = await axios.post('posts', post)
-        dispatch({type: ADD, payload})
-        clear()
-        hide()
+        try{
+            setCss('text-success')
+            const payload = await postRequest('posts', post)
+            dispatch({type: ADD, payload})
+            clear()
+            hide()
+        }catch ({response:{data:{message:err}}}) {
+            console.error(err)
+            setCss('text-danger')
+            return flash(err)
+        }
     }
+    , [hide,clear,dispatch,form,flash,setCss,username,user, course._id]) // eslint-disable-line react-hooks/exhaustive-deps
     return [submit, msg];
 }
 
-const CreatePost = ({course, visible, hide}) => {
+const CreatePost = ({course, hide}) => {
     const [form, onChange, clear] = useForm({title: '', content: ''})
-    const [submit, msg] = useSubmitPost(course, form, clear,hide);
-    const [toRender, flash, setCss] = useFlash()
-    return <>s
-        <Modal visible={visible} hide={hide}>
+    const [submit, msg] = useSubmitPost(course, form, clear, hide);
+    return <>
+        <Modal visible={true} hide={hide}>
             <form className={'CreatePost '} onSubmit={submit}>
-
+                {course._id}
                 <div className={'CreatePost'}> {msg}</div>
                 <label className={'CreatePost '} htmlFor="post-title">Post Title</label>
-                <input  id="post-title" className={'CreatePost '}
-                        onChange={onChange} value={form.title}
+                <input id="post-title" className={'CreatePost '}
+                       onChange={onChange} value={form.title}
                        name={'title'}
                        type="text"/>
 
