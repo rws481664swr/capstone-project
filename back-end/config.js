4b99648225e0d1b6513ff3e50505f63931b90d90
sam raphael <samraphael@gmail.com>
2023-05-15 10:48:15 -0400

    "upgraded" config file and retooled mock-data

--- back-end/config.js
+++ back-end/config.js
@@ -2,19 +2,32 @@
 import { fileURLToPath } from "url";
 const TEST_DB = "mongodb://127.0.0.1:27017/test_db";
 const LOCAL_DEV_DB = "mongodb://127.0.0.1:27017/edu_db";
+
+const{
+  argv:[,mainModule],
+  env:{
+    NODE_ENV,
+  SECRET_KEY:SECRET,
+  EDU_PORT,
+  EDU_DB}
+}= process
+
+
 export let DB_URI =
-  process.env.NODE_ENV === "test"
+  NODE_ENV === "test"
     ? TEST_DB
-    : process.env.EDU_DB || LOCAL_DEV_DB;
+    : EDU_DB 
+    || LOCAL_DEV_DB;
 
-export const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 15;
-export const SECRET_KEY = process.env.SECRET_KEY || "SECRET";
-export const PORT = process.env.EDU_PORT || 3001;
 
-const [, mainModule] = process.argv;
+export const BCRYPT_WORK_FACTOR = NODE_ENV === "test" ? 1 : 15;
+export const SECRET_KEY = SECRET || "SECRET";
+export const PORT = EDU_PORT || 3001;
+
 export const testIsMainModule = (importmeta) =>
   mainModule === fileURLToPath(importmeta);
-export const isTest = process.env.NODE_ENV === "test";
+export const isTest = NODE_ENV === "test";
+
 export const printReport = () =>
   console.log(`
 DB_URI: ${DB_URI}
