939070f08c1f5cae863b3fb4e6fc59b38e7168d5
sam raphael <samraphael@gmail.com>
2023-05-22 13:37:30 -0400

    added alert when network connection cannot reach backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,6 +3,7 @@ import {BASE_URL} from "./config";
 import {useGlobalContext} from "./state/contexts/GlobalContext";
 import {useCallback, useEffect, useMemo} from "react";
 
+const NETWORK_ERROR = 'ERR_NETWORK'
 const useAxios = () => {
 
     const {token:tokenRef} = useGlobalContext()
@@ -23,36 +24,60 @@ const useAxios = () => {
     }),[tokenRef])
 
 
-
-
    return useMemo (
-
        ()=> {
-           async function get(resource, id = '', query = {}) {
-               const {data} = await axios.get(`${BASE_URL}/${resource}/${id}`, config(query))
 
-               return data;
+           async function get(resource, id = '', query = {}) {
+               try {
+                   const {data} = await axios.get(`${BASE_URL}/${resource}/${id}`, config(query))
+                   return data;
+
+               }catch (e) {
+                   if (e.code===NETWORK_ERROR){
+
+                       return alert(`Site is down.`)
+                   }
+                   throw e
+               }
            }
 
-
            async function post(resource, body) {
-               const {data} = await axios.post(`${BASE_URL}/${resource}`, body, config())
-               return data
+               try {
+                   const {data} = await axios.post(`${BASE_URL}/${resource}`, body, config())
+                   return data
+               }catch (e) {
+                   if (e.code===NETWORK_ERROR){
+                       return alert(`Site is down.`)
+                   }
+                   throw e
+               }
            }
 
            async function put(resource, id = '', body) {
-               const {data} = await axios.put(`${BASE_URL}/${resource}`, body, config())
-               return data
+               try {
+                   const {data} = await axios.put(`${BASE_URL}/${resource}`, body, config())
+                   return data
+               }catch (e) {
+                   if (e.code===NETWORK_ERROR){
+                       return alert(`Site is down.`)
+                   }
+                   throw e
+               }
            }
 
            async function delete_(resource, id = '') {
-               const {data} = await axios.delete(`${BASE_URL}/${resource}/${id}`, config())
-               return data;
+               try {
+                   const {data} = await axios.delete(`${BASE_URL}/${resource}/${id}`, config())
+                   return data;
+               }catch (e) {
+                   if (e.code===NETWORK_ERROR){
+                       return alert(`Site is down.`)
+                   }
+                   throw e
+               }
            }
-
 
            return {post, put, delete: delete_, get}
        },[config])
-
 }
 export default useAxios
