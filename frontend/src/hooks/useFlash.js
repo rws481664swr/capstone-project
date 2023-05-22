caa9b05785b0b76b3872b2b3b2be10088558d3ee
sam raphael <samraphael@gmail.com>
2023-05-22 13:31:45 -0400

    code cleanup and small improvements

--- frontend/src/hooks/useFlash.js
+++ frontend/src/hooks/useFlash.js
@@ -1,10 +1,14 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
 const useFlash = (_css='') => {
     const [css, setCss] = useState(_css)
     const [msg, flash] = useState('')
     const toRender=msg ? <span className={css}>{msg}</span> : ''
-    return [toRender, flash, setCss]
+    const danger =  useCallback(() => setCss('text-danger'),
+        [setCss])
+    const success = useCallback(   () => setCss('text-success'),
+        [setCss])
+    return [toRender, flash, setCss, {danger, success}]
 }
 
 export default useFlash
--- frontend/src/hooks/useGet.js
+++ frontend/src/hooks/useGet.js
@@ -7,8 +7,6 @@ import useAxios from '../api'
  * useGet is a hook that returns the result of a get request to the api
  */
 const useGet = (resource, {id = '', init = null, query = null}) => {
-
-
     const {get} = useAxios()
     const [state, setState] = useState(init)
     useEffect(() => {
@@ -16,7 +14,7 @@ const useGet = (resource, {id = '', init = null, query = null}) => {
             const data = await get(resource, id, query || {});
             setState(data)
         })()
-    }, [resource, id]) // eslint-disable-line react-hooks/exhaustive-deps
+    }, [resource, id, query])
     return state
 }
 export default useGet
