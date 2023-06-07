c779d3b275e794c43b941bedde18805be9881af3
sam raphael <samraphael@gmail.com>
2023-06-07 10:21:32 -0400

    added support for mongo atlas

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose'
 import {DB_URI} from '../config.js'
+import {Users} from "./schemas/models.js";
 
 export * from './schemas/models.js'
 
+const {MONGO_PASSWORD,MONGO_USER} = process.env
 export const connect = async () => {
-    try{
-        return await mongoose.connect(DB_URI)
+    try{let conn
+        conn = await mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@edu-db.xjvpbbc.mongodb.net/?retryWrites=true&w=majority`)
+        return conn
 }catch (e) {
-        console.log(e.message)
-        throw e
+        try{
+            console.log('fallback to local db')
+            return await mongoose.connect(DB_URI)
+        }catch (e) {
+            console.log(e.message)
+            throw e
+        }
     }
 }
 
