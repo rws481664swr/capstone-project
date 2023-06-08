2b8817c3352661d49178a9c6b5322e7346cdb0e4
sam raphael <samraphael@gmail.com>
2023-06-08 10:18:06 -0400

    memoized icon buttons + css tweaks

--- frontend/src/components/General/Button/IconButtons/IconButtons.js
+++ frontend/src/components/General/Button/IconButtons/IconButtons.js
@@ -2,6 +2,7 @@ import './IconButtons.css';
 import React from "react";
 import {faBan, faEdit, faMapPin, faThumbTack, faTrash} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {useGlobalContext} from "../../../../state/contexts/GlobalContext";
 export const EditButton = React.memo(
     ({className = '', setEditMode, editMode}) =>
 
@@ -13,14 +14,17 @@ export const EditButton = React.memo(
             ? <FontAwesomeIcon icon={faBan}/>
             : <FontAwesomeIcon icon={faEdit}/>}
     </button>)
-export const PinButton = React.memo(({className = '', canPin, togglePin, pinned}) =>
-
+export const PinButton = React.memo(({className = '', canPin, togglePin, pinned}) => {
+    const {role}= useGlobalContext()
+    return (
     <button
         data-testid={'PinButton'}
-        className={`${className} PinButton IconButton ${pinned ? 'text-primary':''} `}
-            onClick={canPin ? togglePin :()=>{}}>
-         <FontAwesomeIcon icon={faThumbTack}/>
+        className={`${className} ${role==="TEACHER"  ? 'pointerOnHover':''} PinButton IconButton ${pinned ? 'text-primary' : ''} `}
+        onClick={canPin ? togglePin : () => {
+        }}>
+        <FontAwesomeIcon icon={faThumbTack}/>
     </button>)
+})
 
 export const DeleteButton = React.memo(({className = '', onClick}) =>
     <button
