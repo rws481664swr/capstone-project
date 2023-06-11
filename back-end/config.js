91d634402ade8015d0d128c237ccf9067cb30fb2
sam raphael <samraphael@gmail.com>
2023-06-11 13:33:00 -0400

    backend config update

--- back-end/config.js
+++ back-end/config.js
@@ -1,11 +1,18 @@
 /* c8 ignore start */
 import {fileURLToPath} from "url";
+import getDB_URI from "./DB_URI.js";
 
 export const TEST_DB = "mongodb://127.0.0.1:27017/test_db";
 export const LOCAL_DEV_DB = "mongodb://127.0.0.1:27017/edu_db";
-const {} = process.env
+
+if (!process.env.PORT) {
+    if (process.env.EDU_PORT) process.env.PORT = process.env.EDU_PORT
+    else process.env.PORT = 3001
+
+}
+
 const {
-    argv: [, mainModule, flag,arg],
+    argv: [, mainModule, flag, arg],
     env: {
         NODE_ENV,
         SECRET_KEY: SECRET,
@@ -13,45 +20,26 @@ const {
         EDU_DB,
         MONGO_PASSWORD,
         MONGO_USER,
-        EDU_DB_DOMAIN
+        EDU_DB_DOMAIN,
+        PORT
     }
 } = process
 
 export const TEST_PORT = 3002;
 
-export const DB_URI = (() => {
-    if(flag==='--db') {
-        switch (arg){
-            case 'local':
-                return LOCAL_DEV_DB
-            case 'test':
-                return TEST_DB
-            case 'cloud':
-                console.log(`attempting to connect to remote DB as user ${MONGO_USER} at ${EDU_DB_DOMAIN}`)
-                return `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${EDU_DB_DOMAIN}/?retryWrites=true&w=majority`
-        }
-    }
-    const ATLAS = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${EDU_DB_DOMAIN}/?retryWrites=true&w=majority`
-
-
-    switch (NODE_ENV) {
-        case "test":
-            return TEST_DB
-        case "development":
-            return LOCAL_DEV_DB
-        case "production":
-        case 'cloud':
-            console.log(`attempting to connect to remote DB as user ${MONGO_USER} at ${EDU_DB_DOMAIN}`)
-            return ATLAS
-        default:
-            return LOCAL_DEV_DB
-    }
-})()
-
+export const DB_URI = getDB_URI({
+    flag,
+    arg,
+    NODE_ENV,
+    MONGO_USER,
+    MONGO_PASSWORD,
+    EDU_DB_DOMAIN,
+    EDU_DB
+})
+export {PORT}
 
 export const BCRYPT_WORK_FACTOR = NODE_ENV === "test" ? 1 : 13;
 export const SECRET_KEY = SECRET || "SECRET";
-export const PORT = EDU_PORT || 3001;
 
 export const testIsMainModule = (importmeta) =>
     mainModule === fileURLToPath(importmeta);
@@ -66,3 +54,4 @@ PORT: ${PORT}${isTest ? "\nTest Mode: on" : ""}
 `);
 
 /* c8 ignore stop */
+console.log('config.js')
