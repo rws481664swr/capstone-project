8a1201cbc76c6a5ce21664829bf2c46e243115d1
sam raphael <samraphael@gmail.com>
2023-06-12 18:00:07 -0400

    readme created

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 const {  API_URL} = process.env
-export let BASE_URL = /*process.env.DEV_API*/ //|| 'https://edu-comm.herokuapp.com'
+export let BASE_URL = /*process.env.DEV_API*/ //||
+// 'https://edu-comm.herokuapp.com'
      'http://localhost:3001'
